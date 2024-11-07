565064391a3d2ed6a53f8392a832811c7050b8fd
somto idafum <idafum@example.com>
2024-11-07 16:15:47 -0600

    add drag and drop behaviour

--- Pages/PackCreationPage.xaml.cs
+++ Pages/PackCreationPage.xaml.cs
@@ -1,4 +1,5 @@
 using Combat_Critters_2._0.ViewModels;
+using CombatCrittersSharp.objects.card.Interfaces;
 using CommunityToolkit.Maui.Views;
 
 namespace Combat_Critters_2._0.Pages
@@ -9,7 +10,33 @@ namespace Combat_Critters_2._0.Pages
         public PackCreationPage(string packType)
         {
             InitializeComponent();
-            BindingContext = new PackCreationViewModel(packType); //
+            var viewModel = new PackCreationViewModel(packType);
+            BindingContext = viewModel;
+        }
+        private void OnCardDragStarting(object sender, DragStartingEventArgs e)
+        {
+            if (sender is Element element && element.BindingContext is ICard draggedCard)
+            {
+                e.Data.Properties.Add("DraggedCard", draggedCard);
+            }
+        }
+
+        private void OnCardDropCompleted(object sender, DropEventArgs e)
+        {
+            if (e.Data.Properties.TryGetValue("DraggedCard", out object draggedCardObj) &&
+                draggedCardObj is ICard draggedCard)
+            {
+                var viewModel = BindingContext as PackCreationViewModel;
+                if (viewModel != null && viewModel.GameCards.Count < viewModel.CardLimit)
+                {
+                    viewModel.GameCards.Remove(draggedCard);
+                    // Add to SelectedCards or other relevant collection in your ViewModel
+                }
+                else
+                {
+                    DisplayAlert("Limit Reached", $"You can only add {viewModel.CardLimit} cards to this pack.", "OK");
+                }
+            }
         }
 
     }
--- ViewModels/PackCreationViewModel.cs
+++ ViewModels/PackCreationViewModel.cs
@@ -1,9 +1,50 @@
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using Combat_Critters_2._0.Services;
+using CombatCrittersSharp.exception;
+using CombatCrittersSharp.objects.card;
+using CombatCrittersSharp.objects.card.Interfaces;
 
 namespace Combat_Critters_2._0.ViewModels
 {
     public class PackCreationViewModel : INotifyPropertyChanged
     {
+        private readonly BackendService _backendService;
+
+        public bool IsDraggable { get; set; } = true; // Control drag-and-drop functionality
+
+        private bool _hasCards;
+        public bool HasCards
+        {
+            get => _hasCards;
+            set
+            {
+                _hasCards = value;
+                OnPropertyChanged(nameof(HasCards));
+            }
+        }
+
+        private bool _isLoading;
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set
+            {
+                _isLoading = value;
+                OnPropertyChanged(nameof(IsLoading));
+            }
+        }
+        private ObservableCollection<ICard> _gameCards;
+        public ObservableCollection<ICard> GameCards
+        {
+            get => _gameCards;
+            set
+            {
+                _gameCards = value;
+                OnPropertyChanged(nameof(GameCards));
+            }
+        }
+
         private string _packType;
         public string PackType
         {
@@ -41,10 +82,74 @@ namespace Combat_Critters_2._0.ViewModels
 
         public PackCreationViewModel(string packType)
         {
-            PackType = packType;
+            _backendService = new BackendService(ClientSingleton.GetInstance("http://api.combatcritters.ca:4000"));
+            _packType = packType;
+            _description = "";
+            _gameCards = new ObservableCollection<ICard>();
+            HasCards = false;
+
             //LoadAvailableCards();
             UpdateDisplay();
+            //start Loading the user cards.
+            Task.Run(async () => await InitializeViewModelAsync());
+
         }
+
+        private async Task InitializeViewModelAsync()
+        {
+            await LoadGameCards();
+        }
+
+        /// <summary>
+        /// This method uses th backend services to load all game cards
+        /// </summary>
+        /// <returns>Task</returns>
+        public async Task LoadGameCards()
+        {
+            IsLoading = true;
+            bool hasCards = false; // function scoped variable
+            try
+            {
+                CardQueryBuilder filteredBuild = new CardQueryBuilder();
+                //filteredBuild.SetOwned(true);
+                var cards = await _backendService.GetCardsAsync(filteredBuild.Build());
+                Console.WriteLine($"Received {cards?.Count} cards from backend");
+                if (cards != null && cards.Count > 0)
+                {
+
+                    // Application.Current?.Dispatcher.Dispatch(() =>
+                    // 
+                    GameCards = new ObservableCollection<ICard>(cards.Select(stack => stack.Item).ToList());
+                    hasCards = true;
+                    // });
+
+                    Console.WriteLine($"Number of cards loaded: {GameCards.Count}");
+                }
+                else
+                {
+                    //Game has no Cards
+                    GameCards.Clear();
+
+                }
+
+            }
+            catch (RestException)
+            {
+                if (Application.Current?.MainPage != null)
+                    await Application.Current.MainPage.DisplayAlert("Error", "Failed to load game cards. Please try again.", "OK");
+            }
+
+            finally
+            {
+                //Set HasCards based on result of operation
+                HasCards = hasCards;
+                IsLoading = false;
+            }
+        }
+
+        /// <summary>
+        /// Update the display base on the pack type
+        /// </summary>
         private void UpdateDisplay()
         {
             switch (PackType)
