794c4d7bacc93866d5e2d48b792a65eb97ca78a5
somto idafum <idafum@example.com>
2024-11-28 20:51:29 -0600

    pack name entry

--- Pages/PackCreationPage.xaml.cs
+++ Pages/PackCreationPage.xaml.cs
@@ -6,11 +6,24 @@ namespace Combat_Critters_2._0.Pages
 {
     public partial class PackCreationPage : ContentPage
     {
+        PackCreationViewModel _viewModel;
         public PackCreationPage()
         {
             InitializeComponent();
+            _viewModel = new PackCreationViewModel();
             //BindingContext = new PackCreationViewModel();
         }
+
+        /// <summary>
+        /// Update the Pack name when Pack name entry is completed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnPackNameEntryCompleted(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
+
         // private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         // {
         //     if (e.CurrentSelection.Count > 0 && e.CurrentSelection[0] is ICard selectedCard)
