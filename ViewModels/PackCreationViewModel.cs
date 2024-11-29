198dc285797c28c177a9c89c44a488eaae07072a
somto idafum <idafum@example.com>
2024-11-29 15:07:16 -0600

    add UI for adding rarities and weights

--- ViewModels/PackCreationViewModel.cs
+++ ViewModels/PackCreationViewModel.cs
@@ -59,6 +59,122 @@ namespace Combat_Critters_2._0.ViewModels
                 OnPropertyChanged(nameof(GamePackImagesURL));
             }
         }
+
+        //RARITY AND WEIGHTS **This is repetitive. But i'll leave it as it :)**
+        private int? _slot1Rarity;
+        public int? Slot1Rarity
+        {
+            get => _slot1Rarity;
+            set
+            {
+                _slot1Rarity = value;
+                OnPropertyChanged(nameof(Slot1Rarity));
+            }
+        }
+
+        private int? _slot1Weight;
+        public int? Slot1Weight
+        {
+            get => _slot1Weight;
+            set
+            {
+                _slot1Weight = value;
+                OnPropertyChanged(nameof(Slot1Weight));
+            }
+        }
+
+        //SLOT 2
+        private int? _slot2Rarity;
+        public int? Slot2Rarity
+        {
+            get => _slot2Rarity;
+            set
+            {
+                _slot2Rarity = value;
+                OnPropertyChanged(nameof(Slot2Rarity));
+            }
+        }
+
+        private int? _slot2Weight;
+        public int? Slot2Weight
+        {
+            get => _slot2Weight;
+            set
+            {
+                _slot2Weight = value;
+                OnPropertyChanged(nameof(Slot2Weight));
+            }
+        }
+
+        //SLOT 3
+        private int? _slot3Rarity;
+        public int? Slot3Rarity
+        {
+            get => _slot3Rarity;
+            set
+            {
+                _slot3Rarity = value;
+                OnPropertyChanged(nameof(Slot3Rarity));
+            }
+        }
+
+        private int? _slot3Weight;
+        public int? Slot3Weight
+        {
+            get => _slot3Weight;
+            set
+            {
+                _slot3Weight = value;
+                OnPropertyChanged(nameof(Slot3Weight));
+            }
+        }
+
+        //SLOT 4
+        private int? _slot4Rarity;
+        public int? Slot4Rarity
+        {
+            get => _slot4Rarity;
+            set
+            {
+                _slot4Rarity = value;
+                OnPropertyChanged(nameof(Slot4Rarity));
+            }
+        }
+
+        private int? _slot4Weight;
+        public int? Slot4Weight
+        {
+            get => _slot4Weight;
+            set
+            {
+                _slot4Weight = value;
+                OnPropertyChanged(nameof(Slot4Weight));
+            }
+        }
+
+        //SLOT 5
+        private int? _slot5Rarity;
+        public int? Slot5Rarity
+        {
+            get => _slot5Rarity;
+            set
+            {
+                _slot5Rarity = value;
+                OnPropertyChanged(nameof(Slot5Rarity));
+            }
+        }
+
+        private int _slot5Weight;
+        public int Slot5Weight
+        {
+            get => _slot5Weight;
+            set
+            {
+                _slot5Weight = value;
+                OnPropertyChanged(nameof(Slot5Weight));
+            }
+        }
+
         public PackCreationViewModel()
         {
             _backendService = new BackendService(ClientSingleton.GetInstance("http://api.combatcritters.ca:4000"));
